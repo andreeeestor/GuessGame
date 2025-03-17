bc451b58574ec4f08efa4e570990e0c3a7f3f9ad
andreeeestor <andrenestor0503@gmail.com>
2025-03-17 19:55:22 -0300

    Initial Commit

---
+++ GuessGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuessGame
+//
+//  Created by André Nestor on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by André Nestor on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
